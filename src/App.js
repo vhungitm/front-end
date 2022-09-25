7e05b3867477fa1f26750c1e82882344770e1bd5
vhungitm <vhungitm@gmail.com>
2022-09-25 10:20:52 +0700

    React app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import { FieldCheck, FieldCheckGroup, FieldSelect } from 'components/Field';
-import { useState } from 'react';
-
 const App = () => {
-  const [rem, setRem] = useState(false);
-
-  return (
-    // <FieldCheckGroup
-    //   className="m-5"
-    //   element={{
-    //     type: 'radio',
-    //     options: [{ value: '', label: 'All' }]
-    //   }}
-    // />
-    <FieldCheck
-      className="m-5"
-      type="radio"
-      label="a"
-      value={rem}
-      onChange={e => setRem(e.target.checked)}
-    />
-  );
+  return <div>App</div>;
 };
 
 export default App;
